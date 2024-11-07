0b54fa4a18e87bc70a95620b1af0c8cb6067d735
vinicius ribeiro <viniciusrad@yahoo.com.br>
2024-11-06 23:40:02 -0300

    wip - User entitysendo usada

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AdminUser } from './entities/admin-user.entity';
+import { User } from './entities/user.entity';
 import { ProprietarioController } from './proprietario/proprietario.controller';
 import { ProprietarioService } from './proprietario/proprietario.service';
 import { ClienteController } from './cliente/cliente.controller';
 import { ClienteService } from './cliente/cliente.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AdminUser])],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [ProprietarioController, ClienteController],
   providers: [ProprietarioService, ClienteService],
   exports: [ProprietarioService, ClienteService]
